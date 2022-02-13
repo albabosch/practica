19dec838bb201398cdd477349acdf3c44ff63edc
Alba Bosch Freire <albaboschfreire@MacBook-Air-de-Llucia.local>
2022-02-13 19:27:54 +0100

    He añadido lista y bool

---
+++ bool.py
@@ -0,0 +1,11 @@
+fi = False
+
+comptador = 0
+while (fi == False):
+    comptador += 1
+    if (comptador == 10):
+        fi = True
+
+    print (comptador)
+
+#comptador=0, quan compt=10, acabar programa. while
---
+++ llistacompra.py
@@ -0,0 +1,7 @@
+compra = ["patates", "olives", "ous"]
+
+for fruta in compra:
+    print(fruta)
+
+for i in range(0,10,2):
+    print(i)
